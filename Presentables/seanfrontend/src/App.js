b87be9412f41828be4e15376791989413af3763e
I426239 <f.florinvalentin@student.fontys.nl>
2021-03-31 14:39:18 +0200

    added frontend recopied and fixed

---
+++ Presentables/seanfrontend/src/App.js
@@ -0,0 +1,50 @@
+import "./App.css";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import ListRecordsComponent from "./Components/ListRecordsComponent";
+import HeaderComponent from "./Components/HeaderComponent";
+import FooterComponent from "./Components/FooterComponent";
+import AddRecordsComponent from "./Components/AddRecordsComponent";
+import UpdateRecordsComponent from "./Components/UpdateRecordsCompnent";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import Grid from "@material-ui/core/Grid";
+
+function App() {
+  return (
+   
+    <div>
+      <Router>
+        <HeaderComponent />
+        <div className="container">
+          <Switch>
+            {" "}
+            {/*<Route path="/" component={App}></Route>*/}
+            <Route path="/records" component={ListRecordsComponent}></Route>
+            <Route path="/add-record" component={AddRecordsComponent}></Route>
+            <Route
+              path="/update-record/:id"
+              component={UpdateRecordsComponent}
+            ></Route>
+            <Grid container>
+              <Grid item xs={7}>
+                <div>
+                  <ListRecordsComponent />
+                </div>
+              </Grid>
+              <Grid item xs={5}>
+                <div>
+                  <AddRecordsComponent />
+                </div>
+              </Grid>
+            </Grid>
+          </Switch>
+        </div>
+        <FooterComponent />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
