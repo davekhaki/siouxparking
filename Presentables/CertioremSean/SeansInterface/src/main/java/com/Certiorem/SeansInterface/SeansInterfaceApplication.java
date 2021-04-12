ee84ee05c36ae37e5f49e8618a937e25b0291a09
I426239 <f.florinvalentin@student.fontys.nl>
2021-04-12 12:18:53 +0200

    working scheduled thing

--- Presentables/CertioremSean/SeansInterface/src/main/java/com/Certiorem/SeansInterface/SeansInterfaceApplication.java
+++ Presentables/CertioremSean/SeansInterface/src/main/java/com/Certiorem/SeansInterface/SeansInterfaceApplication.java
@@ -1,15 +1,22 @@
 package com.Certiorem.SeansInterface;
 
 import com.Certiorem.SeansInterface.Model.ProtoSean;
+import net.sf.javaanpr.imageanalysis.CarSnapshot;
+import net.sf.javaanpr.intelligence.Intelligence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 import java.util.List;
 
+@EnableScheduling
 @SpringBootApplication
 public class SeansInterfaceApplication implements CommandLineRunner {
 
@@ -19,6 +26,24 @@ public class SeansInterfaceApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 
 		SpringApplication.run(SeansInterfaceApplication.class, args);
+
+		System.err.println("something");
+
+		Intelligence intelligence = null;
+		try {
+			intelligence = new Intelligence();
+			String path="src/test/resources/snapshots/test_001.jpg";
+			CarSnapshot carSnapshot=new CarSnapshot(path);
+			String smth=intelligence.recognize(carSnapshot);
+			System.err.println(smth);
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
