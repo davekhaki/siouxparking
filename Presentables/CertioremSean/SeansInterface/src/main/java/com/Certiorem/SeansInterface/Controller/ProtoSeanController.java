2e916d9e244d1da2d77b4926e3eb3226b9059430
Menderes <61561832+mendokral@users.noreply.github.com>
2021-05-05 16:09:18 +0200

    Commented out search functionality for now in API. It broke getAllRecords method.

--- Presentables/CertioremSean/SeansInterface/src/main/java/com/Certiorem/SeansInterface/Controller/ProtoSeanController.java
+++ Presentables/CertioremSean/SeansInterface/src/main/java/com/Certiorem/SeansInterface/Controller/ProtoSeanController.java
@@ -4,8 +4,6 @@ import com.Certiorem.SeansInterface.Exception.ProtoSeanException;
 import com.Certiorem.SeansInterface.Model.ProtoSean;
 import com.Certiorem.SeansInterface.Repository.ProtoSeanRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,22 +25,23 @@ public class ProtoSeanController {
     @GetMapping("/records")
     public List<ProtoSean> getAllRecords(String keyword, String type){
         ProtoSean protoSean = new ProtoSean();
-        ExampleMatcher matching = ExampleMatcher.matching();
-
-        if ("1".equals(type)) {
-            protoSean.setVisitor(keyword);
-            matching = matching.withMatcher("visitor", ExampleMatcher.GenericPropertyMatchers.contains());
-        } else if ("2".equals(type)) {
-            protoSean.setNumberPlate(keyword);
-            matching = matching.withMatcher("numberPlate", ExampleMatcher.GenericPropertyMatchers.contains());
-        } else if ("3".equals(type)) {
-            protoSean.setPhnNumber(keyword);
-            matching = matching.withMatcher("phnNumber", ExampleMatcher.GenericPropertyMatchers.contains());
-        }
-
-
-        Example<ProtoSean> example = Example.of(protoSean, matching);
-        return protoSeanRepo.findAll(example);
+//        ExampleMatcher matching = ExampleMatcher.matching();
+//
+//        if ("1".equals(type)) {
+//            protoSean.setVisitor(keyword);
+//            matching = matching.withMatcher("visitor", ExampleMatcher.GenericPropertyMatchers.contains());
+//        } else if ("2".equals(type)) {
+//            protoSean.setNumberPlate(keyword);
+//            matching = matching.withMatcher("numberPlate", ExampleMatcher.GenericPropertyMatchers.contains());
+//        } else if ("3".equals(type)) {
+//            protoSean.setPhnNumber(keyword);
+//            matching = matching.withMatcher("phnNumber", ExampleMatcher.GenericPropertyMatchers.contains());
+//        }
+//
+//
+//        Example<ProtoSean> example = Example.of(protoSean, matching);
+//        return protoSeanRepo.findAll(example);
+        return protoSeanRepo.findAll();
     }
 
     // Add the records
